4a3da6d532e5534bbf768b4f042aeb5c829104dc
Pavel Torbeev <32488372+pavel-trbv@users.noreply.github.com>
2021-10-03 17:24:55 +0300

    Add config and database migrations

--- cmd/main.go
+++ cmd/main.go
@@ -5,16 +5,27 @@ import (
 	"github.com/pavel-trbv/go-todo-app/internal/repository"
 	"github.com/pavel-trbv/go-todo-app/internal/server"
 	"github.com/pavel-trbv/go-todo-app/internal/service"
+	"github.com/spf13/viper"
 	"log"
 )
 
 func main() {
+	if err := initConfig(); err != nil {
+		log.Fatalf("error initializing configs: %s", err.Error())
+	}
+
 	repos := repository.NewRepository()
 	services := service.NewService(repos)
 	handlers := handler.NewHandler(services)
 
 	srv := new(server.Server)
-	if err := srv.Run("8000", handlers.InitRoutes()); err != nil {
+	if err := srv.Run(viper.GetString("port"), handlers.InitRoutes()); err != nil {
 		log.Fatalf("error occured while running http server: %s", err)
 	}
+}
+
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
 }
